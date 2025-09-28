fa4b6c563bab0e81b7fc8e0a152ef48dfe75075b
yesseruser <yesseruseryt@gmail.com>
2025-09-28 15:09:09 +0200

    Add natural logarithm

--- YesserCalculator.BaseOperations/Extension.cs
+++ YesserCalculator.BaseOperations/Extension.cs
@@ -13,6 +13,6 @@ public class Extension : IExtension
     public IEnumerable<IOperation> GetOperationList()
     {
         return [new Addition(), new Subtraction(), new Multiplication(), new Division(),
-            new Power(), new Root(), new Logarithm(), new Factorial()];
+            new Power(), new Root(), new Factorial(), new Logarithm(), new NaturalLogarithm()];
     }
 }
