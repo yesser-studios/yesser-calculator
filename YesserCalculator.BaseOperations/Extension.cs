c5dbc4c7cc2ee7865ea2e0dadfa5e42d18673ec8
yesseruser <yesseruseryt@gmail.com>
2025-09-28 14:52:02 +0200

    Add Factorial

--- YesserCalculator.BaseOperations/Extension.cs
+++ YesserCalculator.BaseOperations/Extension.cs
@@ -13,6 +13,6 @@ public class Extension : IExtension
     public IEnumerable<IOperation> GetOperationList()
     {
         return [new Addition(), new Subtraction(), new Multiplication(), new Division(),
-            new Power(), new Root(), new Logarithm()];
+            new Power(), new Root(), new Logarithm(), new Factorial()];
     }
 }
