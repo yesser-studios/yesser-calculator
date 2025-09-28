8958bfc9e8a4ae2fcb75297b569dfbb63b3e9988
yesseruser <yesseruseryt@gmail.com>
2025-09-28 14:25:50 +0200

    Add Power and Root operations

--- YesserCalculator.BaseOperations/Extension.cs
+++ YesserCalculator.BaseOperations/Extension.cs
@@ -12,6 +12,7 @@ public class Extension : IExtension
 
     public IEnumerable<IOperation> GetOperationList()
     {
-        return [new Addition(), new Subtraction(), new Multiplication(), new Division()];
+        return [new Addition(), new Subtraction(), new Multiplication(), new Division(),
+            new Power(), new Root()];
     }
 }
